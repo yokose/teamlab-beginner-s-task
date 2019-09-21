2f193fd542dd51dc08230b073fdc828134c97e10
kenshin yokose <k.yokose.123@ms.saitama-u.ac.jp>
2019-09-21 15:52:58 +0900

    spring boot 等の勉強作業

--- beginner_task/src/main/java/teamlab/beginner_task/HelloController.java
+++ beginner_task/src/main/java/teamlab/beginner_task/HelloController.java
@@ -1,12 +1,22 @@
 package teamlab.beginner_task;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+
 @RestController
 public class HelloController {
-    @RequestMapping(value="/", method= RequestMethod.GET)
+    @RequestMapping(value="/screen", method= RequestMethod.GET)
+    public ModelAndView screen(ModelAndView mv) {
+        mv.setViewName("screen");
+        return mv;
+    }
+    @RequestMapping(value="/index", method= RequestMethod.GET)
     public ModelAndView index(ModelAndView mv) {
         mv.setViewName("index");
         return mv;
@@ -17,5 +27,21 @@ public class HelloController {
         mv.setViewName("result");
         mv.addObject("message", inputvalue);
         return mv;
+    }
+
+    @RequestMapping("hello")
+    private String hello() {
+        return "SpringBoot!";
+    }
+
+    @RequestMapping("/test/{param}")
+    private String testPathVariable( @PathVariable String param ) {
+        return "受け取ったパラメータ：" + param;
+    }
+
+    @RequestMapping( value = "hogemoge4",
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE )
+    public Resource file() {
+        return new FileSystemResource( new File("C:\\test\\hogemoge.png") );
     }
 }
