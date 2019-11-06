69b896ad4b2a4861ba0248c8777323e80af69366
kenshin yokose <k.yokose.123@ms.saitama-u.ac.jp>
2019-11-07 03:02:24 +0900

    test

---
+++ src/main/java/teamlab/beginner_task/BeginnerTaskApplication.java
@@ -0,0 +1,15 @@
+package teamlab.beginner_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BeginnerTaskApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BeginnerTaskApplication.class, args);
+	}
+
+}
